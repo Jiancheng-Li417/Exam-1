abe82b092b665af6f3fdbd1ae183eb5a5ad548ad
Jiancheng-Li417 <15343519972@163.com>
2024-08-09 16:06:12 +0930

    Add files via upload

---
+++ main-1-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+
+extern double arrayMin(double* array, int size);
+
+int main() {
+    double arr[] = {3.5, 2.1, 4.7, -1.2, 0.0};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    double minValue = arrayMin(arr, size);
+
+    std::cout << "The minimum value in the array is: " << minValue << std::endl;
+
+    return 0;
+}
